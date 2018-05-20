46026c201e9bc94bbebf7ae26210f0e80767dc76
Robyn Moscowitz <robyn.moscowitz@gmail.com>
2018-05-20 11:03:32 -0500

    Duplicate http client

--- src/app/providers/app.provider.ts
+++ src/app/providers/app.provider.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import * as _ from 'lodash';
-import {HttpClient} from '@angular/common/http';
 
 import {data} from './mock-data';
 
