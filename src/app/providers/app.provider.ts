ba782f9ce00dacb2a6b996ee66e6770285547624
Robyn Moscowitz <robyn.moscowitz@gmail.com>
2018-05-19 21:02:08 -0500

    Basics for how-are-you-feeling component

--- src/app/providers/app.provider.ts
+++ src/app/providers/app.provider.ts
@@ -1,74 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+enum Emotion {
+  Happy,
+  Neutral,
+  Sad
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppProvider {
-  trees = [
-    'Maple',
-    'Mulberry',
-    'Alder',
-    'Pine',
-    'Birch',
-    'Sycamore',
-    'Hickory, Pecan',
-    'Cottonwood',
-    'Hackberry',
-    'Oak',
-    'Hazelnut',
-    'Willow',
-    'Cedar',
-    'Linden',
-    'Ash',
-    'Elm',
-    'Walnut',
-    'Sweet Gum',
-    'Other Tree Pollen/Unknown',
-  ];
+class AppProvider {
+  trees = ['Maple', 'Mulberry', 'Alder', 'Pine', 'Birch', 'Sycamore', 'Hickory', 'Cottonwood', 'Hackberry', 'Oak', 'Hazelnut', 'Willow', 'Cedar', 'Linden', 'Ash', 'Elm', 'Walnut', 'Sweet Gum', 'Other',  ];
+  weeds = ['Ragweed', 'Plantain', 'Sage', 'Sheep Sorel', 'Aster', 'Cattail', 'Amaranth', 'Nettle', 'Sedge', 'Other',  ];
+  molds = ['Algae', 'Erysiphe', 'Alternaria', 'Aspergillus', 'Ascopores', 'Periconia', 'Basidiospores', 'Pithomyces', 'Cercospora', 'Rusts', 'Cladosporium', 'Myxomycetes', 'Curvularia', 'Spegazzinia', 'Helminthosporium', 'Stemphilium', 'Epicoccum', 'Tetraploa', 'Nigrospora', 'Torula',  ];
 
-  weeds = [
-    'Ragweed',
-    'Plantain',
-    'Sage',
-    'Sheep Sorel',
-    'Aster',
-    'Cattail',
-    'Amaranth',
-    'Nettle',
-    'Sedge',
-    'Other Weed Pollen',
-  ];
-
-  molds = [
-    'Algae',
-    'Erysiphe',
-    'Alternaria',
-    'Aspergillus',
-    'Ascopores',
-    'Periconia',
-    'Basidiospores',
-    'Pithomyces',
-    'Cercospora',
-    'Rusts',
-    'Cladosporium',
-    'Myxomycetes',
-    'Curvularia',
-    'Spegazzinia',
-    'Helminthosporium',
-    'Stemphilium',
-    'Epicoccum',
-    'Tetraploa',
-    'Nigrospora',
-    'Torula',
-  ];
+  
 
   selectedTrees = [];
   selectedWeeds = [];
   selectedMolds = [];
+  feeling: Emotion;
 
-  constructor() { }
+  constructor() {
+  }
 
-  toggleTree(tree: string) {
+  toggleTree(tree:string) {
     if (!this.selectedTrees.includes(tree)) {
       this.selectedTrees.push(tree);
     } else {
@@ -76,7 +32,7 @@ export class AppProvider {
     }
   }
 
-  toggleWeed(weed: string) {
+  toggleWeed(weed:string) {
     if (!this.selectedWeeds.includes(weed)) {
       this.selectedWeeds.push(weed);
     } else {
@@ -84,11 +40,28 @@ export class AppProvider {
     }
   }
 
-  toggleMold(mold: string) {
+  toggleMold(mold:string) {
     if (!this.selectedMolds.includes(mold)) {
       this.selectedMolds.push(mold);
     } else {
       this.selectedMolds = this.selectedMolds.filter(m => m !== mold);
     }
   }
+
+  setFeeling(emotion: Emotion) {
+    this.feeling = emotion;
+
+    if (emotion === Emotion.Happy) {
+      this.selectedTrees = [];
+      this.selectedWeeds = [];
+      this.selectedMolds = [];
+    } else if (emotion === Emotion.Neutral || emotion === Emotion.Sad) {
+      // TODO - select allergens that are moderate or high today
+    }
+  }
+}
+
+export {
+  Emotion,
+  AppProvider,
 }
