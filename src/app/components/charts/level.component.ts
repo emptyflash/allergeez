c049b396a1b132b8847d495b0a97e209e1d63f98
Robyn Moscowitz <robyn.moscowitz@gmail.com>
2018-05-20 02:10:24 -0500

    A few more styles

--- src/app/components/charts/level.component.ts
+++ src/app/components/charts/level.component.ts
@@ -4,10 +4,10 @@ import { Level } from '../../providers/app.provider';
 @Component({
   selector: 'level',
   template: `
-    <div *ngIf="level === Level.Low" class="low">Low</div>
-    <div *ngIf="level === Level.Moderate" class="moderate">Moderate</div>
-    <div *ngIf="level === Level.High" class="high">High</div>
-    <div *ngIf="level === Level.Extreme" class="extreme">Extremely High</div>
+    <div *ngIf="level === Level.Low" class="level low">Low</div>
+    <div *ngIf="level === Level.Moderate" class="level moderate">Moderate</div>
+    <div *ngIf="level === Level.High" class="level high">High</div>
+    <div *ngIf="level === Level.Extreme" class="level extreme">Extremely High</div>
    `,
   styleUrls: ['./level.component.less']
 })
